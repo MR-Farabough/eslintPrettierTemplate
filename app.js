ae203d9643670707c473377410f8f5c937570687
Michael <119344018+Michael-lgtm@users.noreply.github.com>
2023-04-02 15:54:12 -0500

    ESLINT SET UP. single quotes required

---
+++ app.js
@@ -0,0 +1,11 @@
+const name = 'James';
+
+const person = { first: name };
+
+console.log(person);
+
+const sayHelloLinting = (fName) => {
+  console.log(`Hello linting, ${fName}`);
+};
+
+sayHelloLinting('james');
